6cf411e6eff7994d41f1091abc19898e33815a96
chirag041126 <solanki33153315@gmail.com>
2026-03-22 20:05:20 +0530

    added lc136.py

--- Easy/lc136.java
+++ Easy/lc136.java
@@ -1,4 +1,4 @@
-class Solution {
+class SingleNumberSolution {
     public int singleNumber(int[] nums) {
         int n=nums[0];
         for(int i = 1; i<nums.length;i++){
@@ -7,7 +7,7 @@ class Solution {
         return n;
     }
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        SingleNumberSolution sol = new SingleNumberSolution();
         int[] nums = {2, 2, 1};
         System.out.println(sol.singleNumber(nums)); // Output: 1
     }
