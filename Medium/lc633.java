0fe35ad5f3e18a7da339079ca58f6332cea550c8
chirag041126 <solanki33153315@gmail.com>
2026-07-16 14:42:14 +0530

    Sum of Square Numbers

---
+++ Medium/lc633.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long i = 0;
+        long j = (long) Math.sqrt(c);
+        while(i <= j){
+            long sum = i * i + j * j;
+            if (sum==c){
+                return true;
+            }
+            else if (sum < c){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.judgeSquareSum(5));
+    }
+}
