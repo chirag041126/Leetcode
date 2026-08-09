536c1b012e91ce0139fb9c75313a8c4450b3695a
chirag041126 <solanki33153315@gmail.com>
2026-08-09 11:52:48 +0530

    Solved LeetCode #2 Add Two Numbers in Java

---
+++ Medium/lc2.java
@@ -0,0 +1,82 @@
+public class Main {
+
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+            ListNode dummy = new ListNode(0);
+            ListNode current = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0) {
+
+                int sum = carry;
+
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                carry = sum / 10;
+
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+            }
+
+            return dummy.next;
+        }
+    }
+
+    // Method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Number 342 represented as 2 -> 4 -> 3
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        
+        // Number 465 represented as 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        System.out.print("Result: ");
+        printList(result);
+    }
+}
